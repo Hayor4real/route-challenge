e1d0df96b2cb49894b3d8754a6d549cb352a9af8
Akindele Ayorinde Emmanuel <73663718+Hayor4real@users.noreply.github.com>
2022-08-31 12:43:44 +0200

    added some commits

--- src/PostPage.js
+++ src/PostPage.js
@@ -3,20 +3,28 @@ import { useParams, Link } from 'react-router-dom';
 
 const PostPage = ({ posts, handleDelete }) => {
   const { id } = useParams();
-  const post = posts.find(post => (post.id).toString() === id);
+  const post = posts.find((post) => post.id.toString() === id);
   return (
     <main className='PostPage'>
-         <article className='post'>
-               {post && 
-                 <>
-                 <h2>{post.title}</h2>
-                 <p className='postDate'>{post.datetime}</p>
-                  
-                 
-                 </>
-
-               }
-         </article>
+      <article className='post'>
+        {post && (
+          <>
+            <h2>{post.title}</h2>
+            <p className='postDate'>{post.datetime}</p>
+            <p className='postBody'>{post.body}</p>
+            <button onClick={() => handleDelete(post.id)}>Delete Post</button>
+          </>
+        )}
+        {!post && (
+          <>
+            <h2>Post Not Found</h2>
+            <p>Well, that is disappointing</p>
+            <p>
+              <Link to='/'>Visit Our Homepage</Link>
+            </p>
+          </>
+        )}
+      </article>
     </main>
   );
 };
